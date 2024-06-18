96ec66878ae8a0e20b160343e6fa2f180bb1be56
ShinJeongho <sinnago_jh@naver.com>
2024-06-18 16:04:38 +0900

    s2

---
+++ src/main/java/com/winter/app/student/StudentService.java
@@ -0,0 +1,11 @@
+package com.winter.app.student;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+	public void getList() {
+		System.out.println("service list");
+	}
+}
